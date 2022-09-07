fabb36dc6595698701c8e1cc4de29c93d852ce16
Ted Kerr <kerrtheodore13@gmail.com>
2022-09-06 21:16:37 -0400

    Nearme uses the new API for passing in time

--- frontend/src/Components/NearMe/NearMe.js
+++ frontend/src/Components/NearMe/NearMe.js
@@ -10,11 +10,20 @@ export default function NearMe() {
 	const [searchData, setSearchData] = useState();
 	const [restaurantData, setRestaurantData] = useState();
 
+	const timeToUnix = (time = new Date()) => {
+		return Math.floor(time.getTime() / 1000);
+	}
+
 	const searchHandler = async (event) => {
 		event.preventDefault();
+		if (!searchData || !searchData.location || !searchData.date) {
+			alert("Please enter a location and date");
+			return;
+		}
 		let zipcode = searchData.location;
-		let term = searchData.cuisine;
-		const restaurants = await axios.get(urls.yelp + "?term=" + term + "&location=" + zipcode);
+		let term = (searchData.cuisine ? searchData.cuisine : 'restaurant');
+		let open_at = timeToUnix(new Date(searchData.date)) 
+		const restaurants = await axios.get(urls.yelpUnixSearch + "?term=" + term + "&location=" + zipcode + "&eventUnixTime=" + open_at);
 		await setRestaurantData(restaurants.data);
 	}
 
@@ -28,27 +37,53 @@ export default function NearMe() {
 		});
 	}
 
-	const restarauntCards = function() { console.log(restaurantData); 
+	const restarauntCards = function() {  
 		return restaurantData.map(card => (
-			<div className="card" key={card.restarauntId}>
-				<h5>{card.name}</h5>
-				<img src={card.image_url} alt="restaurant" style={{width: "40px"}} />
-				<div>{card.info}</div>
+			<div className="card" key={card.id}>
+				<div>
+					<h5>{card.name}</h5>
+					<img src={card.image_url} alt="restaurant" style={{width: "40px"}} />
+				</div>
+				<div>
+					<button className="add-restaurant">Add</button>
+					<div className="address">
+						<div>{card.location.address1}</div>
+						<div>{card.location.city}, {card.location.state} {card.location.zipcode}</div> 
+					</div>
+					<div className="restaurant-phone">{card.phone}</div>
+					<div className="categories">
+						{(card.categories.map((e) => (<span className="category" key={e.alias}>{e.title}</span>)))}
+					</div>
+					<button className="restaurant-info">More Info</button>
+				</div>
 			</div>)) 
 	};
 
 
 	return (
 		<div>
-			<div>
-				<input type="text" onChange={handleInputChange} name="location"/>
+			<form>
+				<label for="date">Date for Event: </label>
+				<input type="datetime-local" name="date" onChange={handleInputChange} />
+				<label for="location">Location: </label>
+				<input type="text" onChange={handleInputChange} name="location" required/>
+				<label for="cuisine">Type: </label>
 				<select name="cuisine" onChange={handleInputChange}>
-					<option value="french">french</option>
-					<option value="vietnamese">vietnamese</option>
-					<option value="italian">italian</option>
+					<option value="restaurant">none</option>
+					<option value="french">French</option>
+					<option value="chinese">Chinese</option>
+					<option value="japanese">Japanese</option>
+					<option value="vietnamese">Vietnamese</option>
+					<option value="italian">Italian</option>
+					<option value="greek">Greek</option>
+					<option value="mexican">Mexican</option>
+					<option value="thai">Thai</option>
+					<option value="american">American</option>
+					<option value="pizza">Pizza</option>
+					<option value="burgers">burgers</option>
 				</select>
 				<button id="search-button" onClick={searchHandler}>Let's go</button>
-			</div>
+			</form>
 			<div>
 				{(restaurantData) ? restarauntCards() : (<div></div>)}
 			</div>
