e1140746bf35625b2406af93adc795483a7ef192
abhos <alecbhoskins@gmail.com>
2022-09-07 19:55:22 -0500

    commented out authorization annotations, added eventDTO(empty)

--- java/src/main/java/com/techelevator/controller/Controller.java
+++ java/src/main/java/com/techelevator/controller/Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
-@PreAuthorize("isAuthenticated()")
+//@PreAuthorize("isAuthenticated()")
 public class Controller {
 
     @GetMapping()
--- java/src/main/java/com/techelevator/model/Event.java
+++ java/src/main/java/com/techelevator/model/Event.java
@@ -1,40 +1,16 @@
 package com.techelevator.model;
 
+import lombok.Data;
+
 import java.time.LocalDateTime;
 
+@Data
 public class Event {
-
     private long id;
     private LocalDateTime timeAndDate;
     private int zip;
-
-    public Event(long id, LocalDateTime timeAndDate, int zip) {
-        this.id = id;
-        this.timeAndDate = timeAndDate;
-        this.zip = zip;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getTimeAndDate() {
-        return timeAndDate;
-    }
-
-    public void setTimeAndDate(LocalDateTime timeAndDate) {
-        this.timeAndDate = timeAndDate;
-    }
-
-    public int getZip() {
-        return zip;
-    }
-
-    public void setZip(int zip) {
-        this.zip = zip;
-    }
+    private int hostId;
 }
+
+
+
