5368fa643b36948fdbd1e2b39037dcf1b692380a
abhos <alecbhoskins@gmail.com>
2022-09-16 13:06:16 -0500

    changed components to have proper annotations

--- java/src/main/java/com/techelevator/controller/YelpController.java
+++ java/src/main/java/com/techelevator/controller/YelpController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
-@PreAuthorize("isAuthenticated()")
+//@PreAuthorize("isAuthenticated()")
 public class YelpController {
     @Autowired
     YelpBusinessService yelpBusinessService;
