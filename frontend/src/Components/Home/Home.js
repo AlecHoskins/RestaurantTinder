d247dfd8080e4413e1a70c87d2994a1edf041cc2
Alyssa Marie Elliott <alyssa.m.elliott@outlook.com>
2022-09-16 20:15:02 -0600

    Date Fromatting for Home

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -19,7 +19,10 @@ function Home(props) {
     let currUser = useSelector(state => state.user.username);
 
     const [events, setEvents] = useState([]);
+    const [upcomingEvents, setUpcomingEvents] = useState(0);
     const userBlob = './yellowblobuser.png'
+    const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const timeOptions = { timeStyle: 'short'};
 
     useEffect(() => {
         document.title = "Restaurant Tinder - Home"
@@ -35,23 +38,33 @@ function Home(props) {
 		if (myEvents) { setEvents(myEvents.data); }
 	}
 
+    const getNumberOfUpcomingEvents = (eventCards) => {
+        const numWeeks = 2;
+        const now = new Date();
+        now.setDate(now.getDate() + numWeeks * 7);
+        let events = 0;
+        eventCards.map((e) => {
+            const newDate = new Date(e.eventDayTime)
+            if(newDate <= now) {
+                events++;
+            }
+        })
+        return events;
+    }
 
     const getMapOfUpcomingEvents = (eventCards) => {
         const numWeeks = 2;
         const now = new Date();
         now.setDate(now.getDate() + numWeeks * 7);
-        console.log(now);
-        console.log(eventCards);
         return (
             eventCards.map((e) => {
                 const newDate = new Date(e.eventDayTime)
-                console.log('HEREEEEE');
                 if(newDate <= now) {
                     return (
                         <div key={e.id} className='upcomingCard'>
                             <Link to={`/eventview/${e.id}`}><button>Event Details {'>'}</button></Link>
                             <h5>{e.eventTitle}</h5>
-                            <div>{newDate.toLocaleDateString('en-US')}</div>
+                            <div>{newDate.toLocaleDateString('en-US', dateOptions)} @ {newDate.toLocaleTimeString('en-US', timeOptions)}</div>
                         </div>
                     ) 
                 }
@@ -68,7 +81,7 @@ function Home(props) {
             <div className='userInfo'>
                 <div className='welcomeCard'>
                     <h3>Welcome, {currUser}</h3>
-                    <h5>You have <span id="yellow">2</span> upcoming events {'>'}</h5>
+                    <h5>You have <span id="yellow">{getNumberOfUpcomingEvents(events)}</span> upcoming events {'>'}</h5>
                     <Link to='/myevents'><button>My Events</button></Link>
                     <Link to='/nearme'><button>New Event</button></Link>
                 </div>
