e1a8fe6547448dcec2635c110065426df83f50c2
Alyssa Marie Elliott <alyssa.m.elliott@outlook.com>
2022-09-19 15:15:04 -0600

    Double Check and Comments for Home

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -8,6 +8,7 @@ import axios from 'axios';
 import './Home.css'
 import {API} from './../../Shared/API'
 
+//Map State to Props
 const mapStateToProps = state => {
 	return {
 		userId: state.user.id,
@@ -20,12 +21,14 @@ const mapStateToProps = state => {
 
 function Home(props) {
     
+    //Set constants and states
     let currUser = useSelector(state => state.user.username);
     const [events, setEvents] = useState([]);
     const userBlob = './yellowblobuser.png'
     const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     const timeOptions = { timeStyle: 'short'};
 
+    //API call to get a user's events
     const loadEvents = useCallback(async() => {
         if (props.userId === null) { return; }
 	    const myEvents = await axios.get(props.urls.getHostEvents + props.userId, API.createAuthorizedHeaders(props.token)).catch((error) => {
@@ -34,19 +37,22 @@ function Home(props) {
 		if (myEvents) { setEvents(myEvents.data); }
 	}, [props.urls.getHostEvents, props.userId]);
 
+    //useEffect to set document title and trigger loadEvents Function
     useEffect(() => {
         document.title = "Restaurant Tinder - Home"
         loadEvents();
       }, [loadEvents])
 
+    //Function to dynamically change the number of Upcoming Events
     const getNumberOfUpcomingEvents = (eventCards) => {
         const numWeeks = 2;
-        const now = new Date();
-        now.setDate(now.getDate() + numWeeks * 7);
+        const today = new Date();
+        const twoWeeks = new Date();
+        twoWeeks.setDate(twoWeeks.getDate() + numWeeks * 7);
         let events = 0;
         eventCards.map((e) => {
             const newDate = new Date(e.eventDayTime)
-            if(newDate <= now) {
+            if(newDate <= twoWeeks && newDate >= today) {
                 events++;
             }
 			return e;
@@ -54,6 +60,7 @@ function Home(props) {
         return events;
     }
 
+    //Function to check the user's events and see if they are within a 2 week period
     const getMapOfUpcomingEvents = (eventCards) => {
         const numWeeks = 2;
         const today = new Date();
@@ -94,7 +101,7 @@ function Home(props) {
             >
                 <div className='welcomeCard'>
                     <h3>Welcome, {currUser}</h3>
-                    <h5>You have <span id="yellow">{getNumberOfUpcomingEvents(events)}</span> upcoming events {'>'}</h5>
+                    <h5>You have <span id="yellow">{getNumberOfUpcomingEvents(events)}</span> upcoming event{getNumberOfUpcomingEvents(events) === 1 ? "" : "s" } {'>'}</h5>
                     <Link to='/myevents'><button>My Events</button></Link>
                     <Link to='/nearme'><button>New Event</button></Link>
                 </div>
